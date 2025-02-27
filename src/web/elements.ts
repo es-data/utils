d4c8606203f7575bf3fbda544576412ffbdc6be8
૮༼⚆︿⚆༽つ <f.a.wildana@gmail.com>
2025-02-27 16:09:54 +0700

    Update src/web/elements.ts

--- src/web/elements.ts
+++ src/web/elements.ts
@@ -1,31 +1,41 @@
 /** HTML tags where it cannot have any child nodes */
-export const voids = [
-    "area",
-    "base",
-    "br",
-    "col",
-    "embed",
-    "hr",
-    "img",
-    "input",
-    "link",
-    "meta",
-    "param",
-    "source",
-    "track",
-    "wbr",
+export const voidTags = [
+  "area",
+  "base",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr",
 ] as const
+
+const voids = new Set(voidTags)
+
+export const isVoid =
+  (tag: string) => voids.has(tag)
 
 /** HTML tags whose contents are not affected by the current document's styles. The position of the replaced element can be affected using CSS, but not the contents of the replaced element itself. */
-export const replaced = [
-    "img",
-    "video",
-    "iframe",
-    "embed",
-    "fencedframe",
+export const replacedTags = [
+  "img",
+  "video",
+  "iframe",
+  "embed",
+  "fencedframe",
 
-    "option",
-    "audio",
-    "canvas",
-    "object",
+  "option",
+  "audio",
+  "canvas",
+  "object",
 ] as const
+
+const nocontents = new Set(replacedTags)
+
+export const allowCSScontent =
+  (tag: string) => !nocontents.has(tag)
